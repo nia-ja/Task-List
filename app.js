e039a96fb0b1bb024e5b175be8c0dd7b91455e7a
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-02-08 22:27:06 -0800

    Added 'Remove Task' and 'Clear All Tasks' functionality. Fixed a bug with adding empty tasks

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ loadEventListeners();
 function loadEventListeners() {
     //add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear all tasks event
+    clearBtn.addEventListener('click', clearTasks);
 }
 
 // Add Task
@@ -20,26 +24,48 @@ function addTask(e) {
     // cheak if value is present
     if(taskInput.value === '') {
         alert('Add a task');
+    } else {
+        //create li element
+        const li = document.createElement('li');
+        //Add a class
+        li.className = 'collection-item';
+        //Create a text node
+        li.appendChild(document.createTextNode(taskInput.value));
+        // Create new link element 
+        const link = document.createElement('a');
+        // Add a class
+        link.className = 'delete-item secondary-content';
+        // Add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        //Append the link to li
+        li.appendChild(link);
+        //Append li as a child to ul
+        taskList.appendChild(li);
+        //Clear input
+        taskInput.value = '';
     }
 
-    //create li element
-    const li = document.createElement('li');
-    //Add a class
-    li.className = 'collection-item';
-    //Create a text node
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new link element 
-    const link = document.createElement('a');
-    // Add a class
-    link.className = 'delete-item secondary-content';
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    //Append the link to li
-    li.appendChild(link);
-    //Append li as a child to ul
-    taskList.appendChild(li);
-    //Clear input
-    taskInput.value = '';
-
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        // Asks user for confirmation
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    // Option1
+    // taskList.innerHTML = '';
+
+    // Option 2, faster
+    //while firstChild is present = while is still something in the list
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild); // removing the first child
+    }
 }
